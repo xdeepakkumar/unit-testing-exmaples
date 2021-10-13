4262453a3ad69fc1f26cae6972321c7dcc80401e
xdeepakkumar <xdeepakkumar355@gmail.com>
2021-10-13 12:34:54 +0530

    database configuration done

--- src/main/java/com/unitTesting/model/Person.java
+++ src/main/java/com/unitTesting/model/Person.java
@@ -1,9 +1,14 @@
 package com.unitTesting.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
 	private String email;
